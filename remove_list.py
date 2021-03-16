5c37b1736f0142315540372fd9aa19a4299bad62
thraddash <theartist@gmail.com>
2021-03-15 18:38:24 -0700

    clean up print output

--- remove_list.py
+++ remove_list.py
@@ -3,22 +3,43 @@
 test_list = [1, 2, 3, 4, 5]
 remove_list = [2, 4, 5]
 
+a='''##############################
+res = [i for i in test_list if i not in remove_list]
+'''
+b='''##############################
+res2 = list(filter(lambda i: i not in remove_list, test_list))
+'''
+c='''##############################
+set1 = set(test_list)
+set2 = set(remove_list)
+res3 = list(set1 - set2)
+'''
+d='''##############################
+for i in remove_list:
+    try:
+        test_list.remove(i)
+    except ValueError:
+        pass
+print(f"remove: {test_list}")
+'''
 ## list comprehension
 res = [i for i in test_list if i not in remove_list]
-
 print(f"test_list = {test_list}")
 print(f"remove_list = {remove_list}\n")
 print(f"list comprehension: {res}")
+print(a)
 
 ## lambda ##
 res2 = list(filter(lambda i: i not in remove_list, test_list))
 print(f"lambda: {res2}")
+print(b)
 
 ## Set, creating a new filtered list of all the elements that are not present in the remove element list
 set1 = set(test_list)
 set2 = set(remove_list)
 res3 = list(set1 - set2)
 print(f"set: {res3}")
+print(c)
 
 ## remove() method takes a single element as an arguement and removes it from the list
 # if the element doesn't exit, it throws a ValueError ##
@@ -28,5 +49,4 @@ for i in remove_list:
     except ValueError:
         pass
 print(f"remove: {test_list}")
-
-#
+print(d)
